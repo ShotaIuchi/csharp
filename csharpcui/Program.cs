1a2523d831a7fcbf16538c751e5f292f80862ef7
s-iuchi <shotaiuchi.company@gmail.com>
2018-06-13 23:25:01 +0900

    vs2015で作成

---
+++ csharpcui/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpcui
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
